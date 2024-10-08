1d2040206f8442aa9f5f8b5065ea96623d9e5f93
Isabel Reinicke <ireinicke@igpp24grad2.local>
2024-10-08 11:42:39 -0700

    clean version

--- hw_2/hw_2_main.m
+++ hw_2/hw_2_main.m
@@ -8,13 +8,19 @@ time = 1;
 X = 4433469.9438;
 Y = 362672.7267;
 Z = 4556211.6409;
+coord_time_ECEF_example = [time X Y Z];
+
+%number of entries
 N = 5;
+
+%preallocate
 coord_time_wgs84 = zeros(N,4);
-coord_time_ECEF = [time X Y Z];
-coord_time_ECEF2 = zeros(N,4);
+coord_time_ECEF = zeros(N,4);
+
+%generate input matrix
 for t = 1:N
-    coord_time_ECEF2(t,:) = coord_time_ECEF;
-    coord_time_ECEF2(t,1) = t;
+    coord_time_ECEF(t,:) = coord_time_ECEF_example;
+    coord_time_ECEF(t,1) = t;
 end
 
 %check for other coordinates
@@ -24,9 +30,10 @@ x_munich =  4177971;
 y_munich =  855800;
 z_munich =  4727455;
 t = t+1;
-coord_time_ECEF2(t,:) = [t x_munich y_munich z_munich];
 
-% coord_time_wgs84(t, :) = xyz2wgs_t(coord_time_ECEF);
-% coord_time_wgs84
-coord_time_wgs84 = xyz2wgs(coord_time_ECEF2)
+%add additional entry to input matrix
+coord_time_ECEF(t,:) = [t x_munich y_munich z_munich];
+
+%calculate WGS84 coordinates for all ECEF entries, keeping the time value
+coord_time_wgs84 = xyz2wgs(coord_time_ECEF)
 
