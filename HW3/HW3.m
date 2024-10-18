75232b98050b9d905e47a2e9af9c3706e6462427
tgstone <thstone@ucsd.edu>
2024-10-17 17:36:13 -0700

    added parts 2 and 3

--- HW3/HW3.m
+++ HW3/HW3.m
@@ -4,15 +4,48 @@ clear all; close all; clc;
 addpath('/Users/tgstone/Documents/UCSD/classes/SIOG239_GNSS/SIOG230_GNS_Geodesy/matlab_utils')
 
 % Path to data file
-rixen = '/Users/tgstone/Documents/UCSD/classes/SIOG239_GNSS/SIOG230_GNS_Geodesy/HW3/data/brdc0160.12n';
+rixen = '/Users/tgstone/Documents/UCSD/classes/SIOG239_GNSS/SIOG230_GNS_Geodesy/products/epgga9.292';
+sp3file = '/Users/tgstone/Documents/UCSD/classes/SIOG239_GNSS/SIOG230_GNS_Geodesy/products/igs20756.sp3';
+
+% confirming with Sp3 file
 
 % Reading Rixen file
 [eph,alpha,beta,dow] = read_rinexn(rixen);
 
+[sp3,sv,excl_sat] = read_sp3(sp3file);
+
+%SP3 file is in 
+% T, X,Y,Z, dt
+prn1_data = sp3.prn1;
+
 % Set time for for Satellite
-t = 518400;
+svn = 1;
+
+% Grabbing all the times, in time of day (not week)
+ts = prn1_data(:,1)+  6*86400;
 
 % Testing function with Satellite 1 and 10
-rho_es = get_satpos(t,1,eph)
-rho_es = get_satpos(t,30,eph)
+% 96 x 3
+rho_es = get_satpos(ts, svn,eph);
+rho_es = rho_es';
+
+
+% Comparing the differences for sp3 
+% 96x3
+XYZ_sp3 = prn1_data(:,2:4);
+
+mag_sp3 = vecnorm(XYZ_sp3,2,2);
+mag_rho = vecnorm(rho_es,2,2);
+
+% Taking size
+[M,N] = size(mag_rho);
+
+diff = mag_sp3/M - mag_rho/M;
+
+figure();
+plot(ts,diff,'-o');
+title('Comparison of Difference magnitude for SP3 and Rho');
+xlabel("time (s)")
+ylabel("Norm Difference (mag_1 - mag_2) (m)")
+
 
