24f7d813e98bdd94c7342cad6fb4aef799cbb84f
tgstone <thstone@ucsd.edu>
2024-10-21 12:58:42 -0700

    current code is working but waiting on response because NaN values are found in P1, P2, L1 and L2 values

--- HW4/HW4.m
+++ HW4/HW4.m
@@ -38,8 +38,8 @@ P1_obs = obs.P1;
 L2_obs = obs.L2;
 P2_obs = obs.P2;
 
-% Filtering fo prn10
-prn = 10;
+% Filtering fo prn10, first using prn2 because prn10 has NaN values
+prn = 2;
 index_pos = find(gps == prn);
 
 % Currently this is providing NaN files
@@ -47,5 +47,23 @@ L1 = L1_obs(:,index_pos);
 L2 = L2_obs(:,index_pos);
 P1 = P1_obs(:,index_pos);
 P2 = P2_obs(:,index_pos);
+
+% Calculating MP1 and MP2
+p1_const = 2/(alpha - 1);
+p2_const = (2*alpha)/(alpha - 1);
+MP1 = P1 - (p1_const + 1).*L1 + p1_const.*L2;
+MP2 = P2 - p2_const.*L1 + (p2_const - 1).*L2;
+
+% Plotting MP1 and MP2 on same graph
+%
+%(\___/)
+%(=^.^=) Currently, if lines 45-49 are correct then there are a bunch of
+%(")_(") NaN values in the file, Need to determine what to do with NaNs
+%
+figure()
+plot(MP1);
+hold on;
+plot(MP2);
+
 
 
