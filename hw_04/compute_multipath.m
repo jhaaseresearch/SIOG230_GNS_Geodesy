3e7a4bb1980d13cba9abb8e0c72c814d1aaf65a8
Isabel Reinicke <ireinicke@igpp24grad2.local>
2024-10-24 14:54:20 -0700

    first try

--- hw_04/compute_multipath.m
+++ hw_04/compute_multipath.m
@@ -3,5 +3,5 @@ function [MP1,MP2] = compute_multipath(f1,f2, L1, L2, P1, P2)
 %   Detailed explanation goes here
 alpha = (f1./f2).^2;
 MP1 = P1 - (2/(alpha - 1) + 1).* L1 + (2./(alpha-1)).*L2;
-MP2 = P2 - ((2.*alpha)./(alpha - 1)).* L1 + (2./(alpha-1) -1 ).*L2;
+MP2 = P2 - ((2.*alpha)./(alpha - 1)).* L1 + ((2.*alpha)./(alpha-1) -1 ).*L2;
 end
