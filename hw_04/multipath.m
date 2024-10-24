87ffeb864a6f2b0df5a1435383c090ee848f2a59
Isabel Reinicke <ireinicke@igpp24grad2.local>
2024-10-24 14:23:35 -0700

    first try

--- hw_04/multipath.m
+++ hw_04/multipath.m
@@ -12,4 +12,7 @@ lambda_2 = c/f2;
 [data,t,prn,apr,hant] = read_rinexo("opmt2920.19o");
 
 idx = find(prn==10);
-compute_multipath(f1,f2, data.L1(:,idx), data.L2(:,idx),data.P1(:,idx), data.P2(:,idx))
+[MP1, MP2] = compute_multipath(f1,f2, lambda_1.*data.L1(:,idx), lambda_2.*data.L2(:,idx),data.P1(:,idx), data.P2(:,idx));
+plot(MP1 -mean(MP1(~isnan(MP1))));
+hold on
+plot(MP2 -mean(MP2(~isnan(MP2))));
