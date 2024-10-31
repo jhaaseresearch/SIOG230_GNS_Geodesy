d953b3d1110c33771d22028279dd6ff9a002782e
Lane <lsouza@ucsd.edu>
2024-10-31 16:36:41 -0700

    HW5

--- HW5/HW5.m
+++ HW5/HW5.m
@@ -14,6 +14,13 @@ eph_all =read_rinexn("brdc2920.19n")
 c=0.299792458*10^9
 el_cutoff= 10
 
+
+%After I will create a loop here with time
+%For each time in the T_vector, calculate the XYZ-get_satpos   
+%N=size(T_vector, 1) 
+%txyz = zeros(N,5
+
+
 %Read the pseudorange data jP and its corresponding epoch of observation, which you convert into seconds of the current day. 
 %Again, I would start by hard–coding the pseudorange data from the rinex file for one epoch. You can later replace this by 
 %the output of function read_rinexo
@@ -27,7 +34,7 @@ ind_time = find(t(:, 1) == 19 & ...
 
 date_time=datetime(t(ind_time,1), t(ind_time,2), t(ind_time,3), t(ind_time,4), t(ind_time,5), t(ind_time,6))
 seconds_time = seconds(date_time - dateshift(date_time, 'start', 'day'))
-
+[Y_t,M_t,DoM_t,DoY_t,GPSW_t,DoGPSW_t,SoGPSW_t,JD_t,DecY_t] = gpsdate(2019,10,19,0,0,0)
 
 %Compute the time of transmission of the data, which is not the same as the time of observation 
 % in the receiver because the signal has traveled the distance jP between the satellite and the 
@@ -40,120 +47,61 @@ jP_all = obs.C1(ind_time, :);
 nonNaN = ~isnan(jP_all);
 sv_ind = find(nonNaN);
 jP=jP_all(sv_ind)'
-ttx = seconds_time - jP / c;
 
 %Use your function get_satpos to calculate (from broadcast ephemerides) the satellite positions 
 % in the ECEF frame and satellite clock errors (see equation 2.16) at time of transmission ttx. 
 % Make sure they are expressed in meters and seconds.
-
+ttx = seconds_time - jP/c;
 %Choose the satellite and calculate the xyz
 svprn=prn(sv_ind)
 M=size(svprn, 1) 
-eph_sel=struct(); 
-
-for sv_i=1:M
-    [Y_t,M_t,DoM_t,DoY_t,GPSW_t,DoGPSW_t,SoGPSW_t,JD_t,DecY_t] = gpsdate(2019,10,19,0,15,0)
-    T_vector= SoGPSW_t
-
-    %For each time in the T_vector, calculate the XYZ-get_satpos   
-    N=size(T_vector, 1) 
-    txyz = zeros(N,4)
-    
-    for i=1:N
-        [x,y,z]=get_satpos(T_vector(i,1),svprn(sv_i),eph_all)
-        txyz(i, :)=[T_vector(i,1),x,y,z]
-    end
-    
-    svprn_number=svprn(sv_i)
-    eph_sel.(['prn', num2str(svprn_number)]) = txyz;    
-end
-
-
-%XYZ_0 = zeros(M,4)
-%Selecting X0, Y0, and Z0 
-%for prn_i=1:M
-%    i_time=find(sp3.(['prn', num2str(svprn(prn_i))])(:,1) == seconds_time);
-%    X0=sp3.(['prn', num2str(svprn(prn_i))])(i_time,2)
-%    Y0=sp3.(['prn', num2str(svprn(prn_i))])(i_time,3)
-%    Z0=sp3.(['prn', num2str(svprn(prn_i))])(i_time,4)
-%    XYZ_0(prn_i,:)=[svprn(prn_i), X0, Y0, Z0]
-%end
-
-%XO, YO, Z0 for satellite 2
-i_time=find(sp3.(['prn', num2str(svprn(prn_i))])(:,1) == seconds_time);
-X0=sp3.prn2(i_time,2)
-Y0=sp3.prn2(i_time,3)
-Z0=sp3.prn2(i_time,4)
-
-%Selecting X, Y and Z 
-XYZ = zeros(M,4)
-for prn_i=1:M
-%    i_time=find(sp3.(['prn', num2str(svprn(prn_i))])(:,1) == seconds_time);
-    X=eph_sel.(['prn', num2str(svprn(prn_i))])(1,2)
-    Y=eph_sel.(['prn', num2str(svprn(prn_i))])(1,3)
-    Z=eph_sel.(['prn', num2str(svprn(prn_i))])(1,4)
-    XYZ(prn_i,:)=[svprn(prn_i), X, Y, Z]
-end
-
-
-%Compute the modeled observables jρ0;
-
-Diff_X= zeros(M, 1)
-Diff_Y= zeros(M, 1)
-Diff_Z= zeros(M, 1)
-rho_0 = zeros(M,1)
-%for i_sv=1:M 
-%    Diff_X(i_sv)= XYZ(i_sv,2) - XYZ_0(i_sv,2) 
-%    Diff_Y(i_sv)= XYZ(i_sv,3) - XYZ_0(i_sv,3) 
-%    Diff_Z(i_sv)= XYZ(i_sv,4) - XYZ_0(i_sv,4)
-%    rho_0(i_sv)=sqrt(Diff_X(i_sv)^2 + Diff_Y(i_sv)^2 +Diff_Z(i_sv)^2)
-%end
-
-for i_sv=1:M 
-    Diff_X(i_sv)= XYZ(i_sv,2) - X0
-    Diff_Y(i_sv)= XYZ(i_sv,3) - Y0
-    Diff_Z(i_sv)= XYZ(i_sv,4) - Z0
-    rho_0(i_sv)=sqrt(Diff_X(i_sv)^2 + Diff_Y(i_sv)^2 +Diff_Z(i_sv)^2)
-end
-
-
-
-%Compute the observation vector by subtracting known quantities from the pseudorange observation:
-% L = j P − iρ0 + c j δ. Warning: satellite clock biases must be added to the measured pseudoranges;
-
-%Assuming no clock biases because I did not added it to the getsapos yet
+%if M > 4 continue: 
+t_data= SoGPSW_t
 c_s=ones(M,1)*c*1e-9
-%For C1 
-delta_t=-0.00032874 
+X0_adj=apr(1,1) %Initial guess
+Y0_adj=apr(1,2) %Initial guess
+Z0_adj=apr(1,3); %Initial guess
+tolerance = 1; 
+maxIter = 100; 
+iter = 0;
+while iter < maxIter
+    Diff_X= zeros(M, 1)
+    Diff_Y= zeros(M, 1)
+    Diff_Z= zeros(M, 1)
+    rho_0 = zeros(M,1)
+    A = zeros(M, 4);
+    jaX = zeros(M, 1);
+    jaY = zeros(M, 1);
+    jaZ = zeros(M, 1);
+    X0_old = X0_adj;
+    Y0_old = Y0_adj;
+    Z0_old = Z0_adj;
+    iter = iter + 1;
+    for i=1:M
+        [x,y,z, delta_t]=get_satpos(t_data,svprn(i),eph_all)
+        Diff_X(i)= x - X0
+        Diff_Y(i)= y - Y0
+        Diff_Z(i)= z - Z0
+        rho_0(i)=sqrt(Diff_X(i)^2 + Diff_Y(i)^2 +Diff_Z(i)^2)
+        jaX(i) = -(Diff_X(i)) / rho_0(i);
+        jaY(i) = -(Diff_Y(i)) / rho_0(i);
+        jaZ(i) = -(Diff_Z(i)) / rho_0(i);
+        L = jP(i) - rho_0(i) + c_s * delta_t
+        A = [jaX, jaY, jaZ, -1*c_s];
+    end
+    X = pinv(A) * L;  
+    %Compute the covariance of the unknowns in ECEF frame;
+    C = pinv(A' * A);
+    X0_adj=X0 + X(1,1);  % Adjustments
+    Y0_adj=Y0 + X(2,1);  % Adjustments
+    Z0_adj=Z0 + X(3,1);  % Adjustments
 
-
-L = jP - rho_0 + c * delta_t
-
-%Compute the partial derivatives and build the design matrix A. Trick: multiply c by 10−9 in 
-% the design matrix in order to avoid numerical instabilities in the inversion. 
-% The receiver clock bias will be output in nanoseconds.
-
-A = zeros(length(jP), 4);
-jaX = zeros(M, 1);
-jaY = zeros(M, 1);
-jaZ = zeros(M, 1);
-
-for j = 1:length(jP)
-    jaX(j) = -(Diff_X(j)) / rho_0(j);
-    jaY(j) = -(Diff_Y(j)) / rho_0(j);
-    jaZ(j) = -(Diff_Z(j)) / rho_0(j);
+    if abs(X0_adj-X0_old) && abs(Y0_adj-Y0_old) && abs(Z0_adj-Z0_old) < tolerance
+        break;
+    end
 end
 
-A = [jaX, jaY, jaZ, -c_s];
-%Invert the design matrix and find the vector of unknowns, or solve the least squares problem 
-% directly. In MATLAB this can be done using functions pinv or lscov;
 
-X = pinv(A) * L;  
-
-%Compute the covariance of the unknowns in ECEF frame;
-C = pinv(A' * A);
-
-X_final = [X0; Y0; Z0] + X(1:3);  % Adjustments
 
 
 
