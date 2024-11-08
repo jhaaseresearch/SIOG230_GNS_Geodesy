fa7f77af8ffec73a5060e38ff99209c71bd53ec0
Phillip Choi <p3choi@ucsd.edu>
2024-11-08 08:31:53 -0800

    hw05 WIP, but main function runs successfully

--- hw05/construct_mats.m
+++ hw05/construct_mats.m
@@ -1,14 +1,15 @@
-function [A, L] = construct_mats(P, sv_arr, ephBrdc, apr, t0, row)
+function [A, L] = construct_mats(P, sv_arr, ephBrdc, apr, non_nan_row, tidx, gps)
 % construct_mats   Construct L and design  matrix A
 %     In Path: get_satpos.m
 %     Input:
 %       - P = obs.C1 from obs, from read_rinexo.m
-%       - sv = satellite PRN array for the non-NaN time t0
-%       - t0 = valid time of transmission
+%       - sv_arr = satellite PRN INDEX array for the non-NaN times
+                % key to remember this is NOT SV NUMBER
+%       - non_nan_row = valid times of transmission
 %       - ephBrdc = ephemerides matrix created from a rinex navigation
 %               file using read_rinexn.m
 %       - apr = approx. positions [X0, Y0, Z0] from read_rinexo.m
-%       - dt = clock err
+%       - gps = gps numbers from read_rinexo.m
 
 X0 = apr(1);
 Y0 = apr(2);
@@ -21,10 +22,12 @@ L = zeros(length(sv_arr), 1);
 A = zeros(length(sv_arr), 4);
 
 for i = 1:length(sv_arr)
+    t0 = non_nan_row(i);
+    % NOTE: sv_arr =/= gps
     % For this specific satellite number j = sv:
     % sat pos is computed at a t_tx, for each satellite
-    sv = sv_arr(i);
-    satpos = get_satpos(t0,sv,ephBrdc,3);
+    sv = gps(sv_arr(i));
+    satpos = get_satpos(t0,sv,ephBrdc,0);
     % output satpos = [X Y Z dt toe drel]
     X = satpos(1);
     Y = satpos(2);
@@ -32,15 +35,17 @@ for i = 1:length(sv_arr)
     dt = satpos(4);
     
     rho0 = sqrt((X-X0)^2 + (Y-Y0)^2 + (Z-Z0)^2);
-    a_x = -(X-X0/rho0);
-    a_y = -(Y-Y0/rho0);
-    a_z = -(Z-Z0/rho0);
-    l = P(row,sv) - rho0 + c*dt;
+    a_x = -((X-X0)/rho0);
+    a_y = -((Y-Y0)/rho0);
+    a_z = -((Z-Z0)/rho0);
+    l = P(tidx,sv_arr(i)) - rho0 + c*dt;
     
     L(i) = l;
     A(i, 1) = a_x;
     A(i, 2) = a_y;
     A(i, 3) = a_z;
-    A(i, 4) = c * 1e-9; % multiply by 10^-9
+    A(i, 4) = -0.299792458; % multiply by 10^-9
+
+    
 end
 
---
+++ hw05/readme.txt
@@ -0,0 +1,7 @@
+HW05 - Phillip Choi Submission
+
+construct_mats.m = function for computing A and L
+
+hw05_working.mlx = main submission, live script for Assignment 2.4.3
+
+hw05_dev.mlx = scratch testing live script for code development
